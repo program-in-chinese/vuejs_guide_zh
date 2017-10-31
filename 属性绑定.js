8fb8c10069cfb2b8f14e82b73b5c5fdb7013b5ba
fromwheretowhere <fromwheretowhere.service@gmail.com>
2017-10-30 17:27:38 -0700

    条件/循环

---
+++ 属性绑定.js
@@ -0,0 +1,6 @@
+var 应用2 = new Vue({
+  el: '#元素id2',
+  data: {
+    动态绑定信息: '页面加载于 ' + new Date().toLocaleString()
+  }
+})
---
+++ 条件.js
@@ -0,0 +1,6 @@
+var 应用3 = new Vue({
+  el: '#元素id3',
+  data: {
+    看得到: true
+  }
+})
